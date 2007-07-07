f328fe0b9c97698bcc45e3beb1255374652dcc7a
margus <margus@b5ae693c-5924-0410-9cb7-bac31be1ea3f>
2007-07-07 20:42:29 +0000

    OUnit is here to stay.

--- src/test_parser.ml
+++ src/test_parser.ml
@@ -1,11 +1,22 @@
+open OUnit
+
 let parse_rule s =
   let lexbuf = Lexing.from_string s in
     Grammar.rule Lexer.token lexbuf
 and parse_conjunct s =
   let lexbuf = Lexing.from_string s in
     Grammar.conjunct Lexer.token lexbuf
-in
 
-  print_endline "\n\nTesting parser";
+let test_parse_terms _ =
+  assert_bool "surprise!" true
 
-  
+let test_parse_lists _ =
+  assert_bool "surprise!" true
+
+let test_parse_rules _ =
+  assert_bool "surprise!" true
+
+let suite = "Parser" >::: [
+  ("test_parse_terms" >:: test_parse_terms);
+  ("test_parse_lists" >:: test_parse_lists);
+  ("test_parse_rules" >:: test_parse_rules)]
